312dec5569b6d52cece3b0ccd0332940a7885e8e
mf.galiakbirov@gmail.com <n19869400>
2022-01-09 21:34:55 +0300

    Life cycle of Activiny

--- app/src/main/java/com/example/learning/MainActivity.kt
+++ app/src/main/java/com/example/learning/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.learning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MyLog", "Start function \"onStart\"")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MyLog", "Start function \"onResume\"")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MyLog", "Start function \"onPause\"")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MyLog", "Start function \"onStop\"")
+    }
 }
+
