c925b9627832a7d5c0eec0657fa91e99d832c995
mf.galiakbirov@gmail.com <n19869400>
2022-01-16 22:32:10 +0300

    add new texView, editText and button

--- app/src/main/java/com/example/learning/MainActivity.kt
+++ app/src/main/java/com/example/learning/MainActivity.kt
@@ -2,19 +2,35 @@ package com.example.learning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import com.example.learning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bindClass : ActivityMainBinding
+    lateinit var bindingClass : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bindClass = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindClass.root)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
 
-        bindClass.bTest.setOnClickListener {
-            bindClass.tvTest.text = "123123132"
+        bindingClass.btResult.setOnClickListener{
+            val resultValue = bindingClass.edValue.text.toString().toInt()
+            when(resultValue){
+                in 0..1000 -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    bindingClass.tvResult.text = getText(R.string.tvResultTo1000)
+                }
+                in 1000..100000 -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    bindingClass.tvResult.text = getText(R.string.tvResultTo100000)
+                }
+                else -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    bindingClass.tvResult.text = getText(R.string.tvResultToOver)
+                }
+            }
         }
     }
 }
